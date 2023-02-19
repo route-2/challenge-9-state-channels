4da09fb165ab2e4c39b6f66d60c238df7efd2d3d
route-2 <ruthurao@gmail.com>
2023-02-20 01:18:13 +0530

    state-channels

---
+++ packages/hardhat/deploy/00_deploy_streamer.js
@@ -0,0 +1,28 @@
+// deploy/00_deploy_streamer.js
+
+const { ethers } = require("hardhat");
+
+module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  await deploy("Streamer", {
+    // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
+    from: deployer,
+    log: true,
+  });
+
+  const streamer = await ethers.getContract("Streamer", deployer);
+
+  console.log("\n 🤹  Sending ownership to frontend address...\n");
+ 
+  const ownerTx = await streamer.transferOwnership("0x657dD02b4EAE6Bf276747bd3B9E54aB034D9F97f")
+
+  console.log("\n       confirming...\n");
+  const ownershipResult = await ownerTx.wait();
+  if (ownershipResult) {
+    console.log("       ✅ ownership transferred successfully!\n");
+  }
+};
+
+module.exports.tags = ["Streamer"];
